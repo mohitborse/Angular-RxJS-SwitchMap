60e428265febfb16fd577c1810535a280407a1ef
mohitborse <mohitborse@hotmail.com>
2022-03-24 15:27:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-cakhet
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-cakhet)
---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  getResponse(): Observable<any> {
+    return Observable.create((observer) => {
+      observer.next({ name: 'Mohit' });
+    });
+  }
+
+  getMResponse(): Observable<any> {
+    return Observable.create((observer) => {
+      observer.next({ name: 'Rohit' });
+    });
+  }
+
+  ngOnInit() {
+    this.getResponse()
+      .pipe(
+        switchMap((x) => {
+          return this.getMResponse().pipe(map((y) => ({ x, y })));
+        })
+      )
+      .subscribe(({ x, y }) => console.log(x, y));
+  }
+}
